9d99d5c08ac8195abb6e7bf5e56c7ffd37fe081c
malkange <geonsoo92@gmail.com>
2024-02-29 23:38:20 +0900

    first commit

---
+++ run_setting_auto.sh
@@ -0,0 +1,4 @@
+pip install -r requirements.txt
+pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117
+pip install torch_scatter-2.1.1+pt113cu117-cp38-cp38-linux_x86_64.whl 
+pip install spconv-cu117==2.3.6
---
+++ scripts/kitti_train.sh
@@ -0,0 +1,13 @@
+export CUDA_VISIBLE_DEVICES=0,1
+
+cd ..
+python -m torch.distributed.launch --nproc_per_node=2 --use_env --master_port=25692 train_distributed.py \
+-c configs/pa_po_kitti_trainval.yaml \
+-l /visionai-postech/eccv24/kitti_logs \
+-w kitti_final \
+-n kitti_pixseg_cont \
+
+
+
+#-n nusc_late_cont_head_attnfea \
+#-x \
